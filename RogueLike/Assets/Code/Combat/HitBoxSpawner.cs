eb6ae69cfe11a5c4b8dc7bd67a6dc31ea0f81d52
Borito185 <boris.annink@gmail.com>
2022-07-13 10:43:12 +0200

    Small hitbox testing

--- RogueLike/Assets/Code/Combat/HitBoxSpawner.cs
+++ RogueLike/Assets/Code/Combat/HitBoxSpawner.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Code;
 using Assets.Code.Networking;
+using Mirror;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HitBoxSpawner : NetworkSingleton<HitBoxSpawner>
@@ -11,14 +13,33 @@ public class HitBoxSpawner : NetworkSingleton<HitBoxSpawner>
 
     public override void OnStartServer()
     {
-        if (CirclePrefab == null)
-            Debug.LogError($"no {nameof(CirclePrefab)} found");
-        if (BoxPrefab == null)
-            Debug.LogError($"no {nameof(BoxPrefab)} found");
+        if (CirclePrefab == null || BoxPrefab == null)
+        {
+            Debug.LogError("no prefab found");
+            return;
+        }
+
+        AddIfNotContained(CirclePrefab);
+        AddIfNotContained(BoxPrefab);
+
+        static void AddIfNotContained(GameObject value)
+        {
+            List<GameObject> prefabs = NetworkManager.singleton.spawnPrefabs;
+            if (!prefabs.Contains(value))
+            {
+                prefabs.Add(value);
+            }
+        }
     }
 
-    public void SpawnCircle(int size, Vector2 position)
+    public void SpawnCircle(GameObject owner, int size, Vector2 position)
     {
-
+        GameObject instance = Instantiate(CirclePrefab);
+        instance.transform.position = position;
+        Vector3 scale = instance.transform.localScale;
+        scale.x *= size;
+        scale.y *= size;
+        instance.transform.localScale = scale;
+        NetworkServer.Spawn(instance, owner);
     }
 }
